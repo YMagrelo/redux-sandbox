58548a9471bab8fd2c071ee46f6765bb268cd609
Yaroslav Magrelo <slava.magrelo@gmail.com>
2021-07-21 13:09:46 +0300

    add bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,43 +1,35 @@
-import { createStore } from 'redux';
-
-const reducer = (state = 0, action) => {
-    switch(action.type) {
-        case 'INC':
-            return state + 1;
-
-        case 'DEC':
-            return state - 1;
-
-        case 'RND':
-            return state + action.payload;
-
-        default: 
-          return state;
-    }
-};
+import { createStore, bindActionCreators } from 'redux';
+import * as actions from './actions';
+import reducer from './reducer';
+console.log(actions)
 
 const store = createStore(reducer);
+const { dispatch } = store;
+
+
+const { 
+    inc, 
+    dec, 
+    rnd
+ } = bindActionCreators(actions, dispatch)
 
 document
   .getElementById('inc')
   .addEventListener('click', () => {
-      store.dispatch({type: 'INC'})
+      inc();
   })
 
   document
   .getElementById('dec')
   .addEventListener('click', () => {
-      store.dispatch({type: 'DEC'})
+    dec();
   })
 
   document
   .getElementById('rnd')
   .addEventListener('click', () => {
       const payload = Math.floor(Math.random() * 10);
-      store.dispatch({
-          type: 'RND',
-          payload
-        })
+      rnd(payload);
   })
 
   const update = () => {
