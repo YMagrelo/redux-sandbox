5d9a82813e1af8f4a754cacd90ba29edd5ff3077
Yaroslav Magrelo <slava.magrelo@gmail.com>
2021-07-22 16:12:31 +0300

    map state and dispatch to props of Counter component

--- src/components/counter.js
+++ src/components/counter.js
@@ -26,16 +26,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
-  const { inc, dec, rnd} = bindActionCreators(actions, dispatch);
-  return {
-    inc: () => inc(),
-    dec: () => dec(),
-    rnd: () => {
-      const randomValue = Math.floor(Math.random() * 10);
-      rnd(randomValue);
-    }
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect(mapStateToProps, actions)(Counter);
